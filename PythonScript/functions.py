ff8c7148bc899ee229647d8cf05f552b5c6f35d3
Daniel Cruz Gonzalez <daniel.cruz.gonzalez@gmail.com>
2018-02-09 17:40:22 -0600

    Error en fecha
    Se corrigio error en la version 3.3 del CFDI donde el campo de fecha cambia por Fecha

--- PythonScript/functions.py
+++ PythonScript/functions.py
@@ -2,18 +2,31 @@ def sortCFDI(source, destination):
 	import os
 	import glob
 	import shutil
+	import time
+	import datetime
+	import sys
 	from xml.dom import minidom
+
 	source.strip()
 	destination.strip()
 	source += "/"
 	destination += "/"
 	errors = {}
+
+	log  = open(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') + ".csv", "w") 
+	#log.write("sep=,")
+	log.write("Estatus,Origen,Destino\n")
 	for filename in glob.glob(source + '*.xml'):
 		rfc = ""
 	 	cfdi = minidom.parse(filename)
 	 	for comprobante in cfdi.getElementsByTagName('cfdi:Comprobante'):
 	 		if comprobante.attributes.has_key("fecha"):
 	 			fecha = comprobante.attributes['fecha'].value.split('-')
+	 		else:
+	 			if comprobante.attributes.has_key("Fecha"):
+	 				fecha = comprobante.attributes['Fecha'].value.split('-')
+	 			else:
+	 				fecha = None
 	 	for receptor in cfdi.getElementsByTagName('cfdi:Emisor'):
 	 		if receptor.attributes.has_key("rfc"):
 	 			rfc = receptor.attributes['rfc'].value
@@ -22,14 +35,20 @@ def sortCFDI(source, destination):
 	 				rfc = receptor.attributes['Rfc'].value
 	 			else:
 	 				print "Error en:" + filename
-	 		if not os.path.isdir(destination + fecha[0]):
-	 			os.mkdir(destination + fecha[0])
-	 		if not os.path.isdir(destination + fecha[0] + '/' + fecha[1]):
-	 			os.mkdir(destination + fecha[0] + '/' + fecha[1])
-	 		if not os.path.isdir(destination + fecha[0] + '/' + fecha[1] + '/' + rfc):
-	 			os.mkdir(destination + fecha[0] + '/' + fecha[1] + '/' + rfc)
+			if not os.path.isdir(destination + rfc):
+	 			os.mkdir(destination + rfc)
+	 		if not os.path.isdir(destination + rfc + '/' + fecha[0]):
+	 			os.mkdir(destination + rfc + '/' + fecha[0])
+	 		if not os.path.isdir(destination + rfc + '/' + fecha[0] + '/' + fecha[1]):
+	 			os.mkdir(destination + rfc + '/' + fecha[0] + '/' + fecha[1])
 	 	try:
-	 		shutil.copy(filename, destination + fecha[0] + '/' + fecha[1] + '/' + rfc)
+	 		shutil.copy(filename, destination + rfc + '/' + fecha[0] + '/' + fecha[1])
+	 		log.write("Success," + filename + "," + destination + rfc + '/' + fecha[0] + '/' + fecha[1] + "\n")
+	 	except IOError as e:
+	 		log.write("Error," + filename + "," + "I/O error({0}): {1}".format(e.errno, e.strerror) + "\n")
+	 		errors[filename] = "I/O error({0}): {1}".format(e.errno, e.strerror)
 	 	except:
-	 		errors[filename] = sys.exc_info()[0]
+	 		log.write("Error," + filename + "," + str(sys.exc_info()[0]) + "\n")
+	 		errors[filename] = str(sys.exc_info()[0])
+	log.close()
 	return errors
